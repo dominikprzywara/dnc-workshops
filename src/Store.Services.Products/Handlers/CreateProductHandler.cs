9de2911ffb83b98e927f58a55e39dfe9182c8135
Dominik Przywara <dominik.przywara@gmail.com>
2018-06-30 12:35:29 +0200

    First message working

---
+++ src/Store.Services.Products/Handlers/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dnc.Common;
+using Dnc.Common.Commands;
+using Store.Messages.Products;
+
+namespace Store.Services.Products.Handlers
+{
+    public class CreateProductHandler : ICommandHandler<CreateProduct>
+    {
+        public Task HandleAsync(CreateProduct command, ICorrelationContext context)
+        {
+            Console.WriteLine($"Create product {command.Name}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
